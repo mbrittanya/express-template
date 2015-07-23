62e2beaea48743bee6159f29859c207627ecf951
brittanya <m.brittanya@gmail.com>
2015-07-22 21:10:59 -0600

    stuff

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
-var app = express();
+var app = express(); //creating our app
 
-app.use(express.static('app'));
+app.use(express.static('app')); /* Need to serve up the 'app' folder (directory) 
+as static assets and needs to be rendered as the specified type */
 
-app.get('*', function (req, res) {
+app.get('*', function (req, res) { // * means any get requests
     res.sendFile(__dirname + '/app/index.html');
 });
 
 app.listen(process.env.PORT || 5000);
+//listen for first variable (if false or not there), then use 5000
+
+
+//To start: npm install
+// : node index.js
+// go to local host 5000
